5f2170e7992fec2fcde31266561345e95d2ceefb
ArchiINT <architype974@gmail.com>
2026-03-30 02:00:30 +0200

    time funtion

--- main/awatch_test.c
+++ main/awatch_test.c
@@ -3,7 +3,7 @@
 #include "st7789_init.h"
 #include "st7789_draw.h"
 #include <stdint.h>
-
+#include <time.h>
 
 
 void app_main(void)
@@ -17,12 +17,31 @@ void app_main(void)
     st7789_fill_color(0x07E0);
         
     vTaskDelay(pdMS_TO_TICKS(1000));
-    st7789_fill_color(0x001F);
+    st7789_fill_color(0x0000);
 
     // draw_circle(120, 120, 50, 0xFFFF, 2);
+   
     draw_string(50, 50, "HELLO!", &Font_16x26, 0xFFFF, 0x0000);
-    while (1)
-    {
 
+    time_t now;
+    struct tm timeinfo;
+    char strftime_buf[64];
+
+    setenv("TZ", "CET-1CEST,M3.5.0/2,M10.5.0/3", 1);
+    tzset();
+
+    while(1)
+    {
+        time(&now);
+        localtime_r(&now, &timeinfo);
+        strftime(strftime_buf, sizeof(strftime_buf), "%d-%m-%Y", &timeinfo);
+        draw_string(20, 80, strftime_buf, &Font_16x26, 0xFFFF, 0x0000);
+
+        strftime(strftime_buf, sizeof(strftime_buf), "%H:%M:%S", &timeinfo);
+        draw_string(20, 110, strftime_buf, &Font_16x26, 0xFFFF, 0x001F);
+        
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
+
+
